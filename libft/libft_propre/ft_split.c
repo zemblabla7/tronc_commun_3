7c02ee5be93dea2fa6f53f0fd903084392e4f934
zemblabla7 <59145252+zemblabla7@users.noreply.github.com>
2022-11-27 15:09:16 +0100

    SPLIT MARCHE

--- libft/libft_propre/ft_split.c
+++ libft/libft_propre/ft_split.c
@@ -6,109 +6,102 @@
 /*   By: casomarr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 14:18:56 by casomarr          #+#    #+#             */
-/*   Updated: 2022/11/25 16:50:13 by casomarr         ###   ########.fr       */
+/*   Updated: 2022/11/27 12:06:28 by casomarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int     word_length(char *str, char c)
+size_t	word_length(char *str, char c)
 {
-    int    i;
-    int letters;
+	size_t	len;
 
-    i = 0;
-    letters = 0;
-    while(str[i])
-    {
-        if (str[i] == c)
-            i++;
-        if (str[i] != c)
-        {
-            while (str[i] != c && str[i] != '\0')
-            {
-                letters++;
-                i++;
-            }
-            break ;
-        }
-    }
-    return (letters);
+	len = 0;
+	while (*str == c) // longeur separateur pour re-set str
+			++str;
+	while (str[len] != c && str[len] != '\0') // longeur mot pour malloc
+		++len;
+	return (len);
 }
 
-int    count_words(char *str, char c)
+size_t	count_words(char *str, char c)
 {
-    int nb_of_words;
-    int i;
+	size_t	nb_of_words;
+	size_t	i;
 
-    nb_of_words = 0;
-    i = 0;
-    while(str[i])
-    {
-        if((str[i] != c && str[i] != '\0') && (str[i + 1] == c || str[i + 1] == '\0'))
-        {
-            nb_of_words++;
-        }
-        i++;
-    }
-    return(nb_of_words);
+	nb_of_words = 0;
+	i = 0;
+	while (str[i])
+	{
+		if ((str[i] != c && str[i] != '\0')
+			&& (str[i + 1] == c || str[i + 1] == '\0'))
+		{
+			nb_of_words++;
+		}
+		i++;
+	}
+	return (nb_of_words);
 }
 
-void write_words(char *dest, char *src, char c)
+void	write_words(char *dest, char *src, char c)
 {
-    int i;
-    int j;
+	size_t	i;
 
-    i = 0;
-    j = 0;
-    while (src[i] == c)
-      i++;
-    while (src[i] != c && src[i] != '\0')
-      dest[j++] = src[i++];
-    dest[j] = '\0';
+	i = 0;
+	while (src[i] != c && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		++i;
+	}
+	dest[i] = '\0';
 }
 
-int    write_split(char **string, char *str, char c)
+int	write_split(char **string, char *str, char c)
 {
-    int    word;
-    int total_nb_of_words;
+	size_t	word;
+	size_t	total_nb_of_words;
+	size_t	i;
 
-    word = 0;
-    total_nb_of_words = count_words(str, c);
-    while (word <= total_nb_of_words) // si je mets direct cla fx count_words ici alors ça deviendra de valeur 2 puis 1 au lieu de 3 car la valeur de str diminue à chaque boucle.
-    {
-            string[word] = malloc(sizeof(char) * (word_length(str, c) + 1));
-            if (string == NULL)
-                return (0);
-            write_words(string[word], str, c);
-            str = str + word_length(str, c) + 1; // str + j = str[j] donc le str envoyé dans write_words ne vaudra plus str[0] mais str[j].
-            word++;
-    }
-    return (1);
+	word = 0;
+	total_nb_of_words = count_words(str, c);
+	while (word < total_nb_of_words) // si je mets direct la fx count_words ici alors ça deviendra de valeur 2 puis 1 au lieu de 3 car la valeur de str diminue à chaque boucle.
+	{
+		string[word] = malloc(sizeof(char) * (word_length(str, c) + 1));
+		if (string[word] == NULL)
+			return (EXIT_FAILURE);
+		i = 0;
+		while (str[i] == c && str[i] != '\0')
+			++i;
+		str += i;
+		write_words(string[word], str, c);
+		str = str + word_length(str, c); // str + j = str[j] donc le str envoyé dans write_words ne vaudra plus str[0] mais str[j].
+		word++;
+	}
+	string[total_nb_of_words] = NULL;
+	return (EXIT_SUCCESS);
 }
 
-char **ft_split(char const *s, char c)
+char	**ft_split(char const *s, char c)
 {
-    char **string;
-    int words;
+	char	**string;
+	size_t	words;
 
-    words = count_words((char *)s, c);
-    string = malloc(sizeof(char *) * (words + 1));
-    if (string == NULL || s == NULL)
-            return (NULL);
-    if (write_split(string, (char *)s, c) == 0)
-    {
-        while (words > 0)
-            free(string[--words]); // on decremente avant pq dans les [] c est tjrs un nb en moins
-        free(string);
-        //string = NULL;
-    }
-        return (string);
+	words = count_words((char *)s, c);
+	string = malloc(sizeof(char *) * (words + 1));
+	if (string == NULL)
+		return (NULL);
+	if (write_split(string, (char *)s, c) == EXIT_FAILURE)
+	{
+		while (words > 0)
+			free(string[--words]); // on decremente avant pq dans les [] c est tjrs un nb en moins
+		free(string);
+		// string = NULL;
+	}
+	return (string);
 }
 
-/* MAIN DE PISCINE modifi� pour essayer a 42
-#include <stdio.h>
-
+// MAIN DE PISCINE modifi� pour essayer a 42
+/*
 int main (int argc, char **argv)
 {
 char **split;
@@ -121,31 +114,50 @@ split = ft_split(argv[1], argv[2]);
 
 while(split[i])
 {
-printf("%s\n", split[i]);
-i++;
+	printf("%s\n", split[i]);
+	i++;
 }
 return 0;
 }
- */
-
-/*
-int main()
-{
-	char s[] = "carolina tiene mucha hambre";
-	char c = ' ';
-
-	//printf("%s\n", *ft_split(s, c));
-
-	int i = 0;
-	char **split;
-	split = ft_split(s, c);
-	while(split[i])
-	//while(i <= 3)
-    {
-        printf("%s\n", split[i]);
-        i++;
-    }
-    return 0;
-}
 */
 
+
+// int main()
+// {
+// 	char s[] = "carolina tiene mucha hambre";
+// 	char c = ' ';
+
+// 	//printf("%s\n", *ft_split(s, c));
+
+// 	int i = 0;
+// 	char **split;
+// 	split = ft_split(s, c);
+// 	while(split[i])
+// 	//while(i <= 3)
+// 	{
+// 		printf("%s\n", split[i]);
+// 		i++;
+// 	}
+// 	return 0;
+// }
+/*
+int main(int ac, char **av)
+{
+	int	i = 0;
+	char **split;
+	//split = ft_split("lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse", ' ');
+	//printf("word_count --> %zu\n", count_words("", 'z'));
+	if (ac > 2)
+	{
+		split = ft_split(av[1], av[2][0]);
+		while(split[i])
+		{
+			printf("%s\n", split[i]);
+			free(split[i]);
+			i++;
+		}
+		free(split);
+	}
+	return (EXIT_SUCCESS);
+}
+*/
