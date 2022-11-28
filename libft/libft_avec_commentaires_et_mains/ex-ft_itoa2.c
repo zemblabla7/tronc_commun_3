fd6ff56fcbd82b8a49b950fc3b4cc5be0f0f9ee0
zemblabla7 <59145252+zemblabla7@users.noreply.github.com>
2022-11-28 17:57:33 +0100

    dernier push lundi soir -- important de revoir ex_ft_itoa2.c

---
+++ libft/libft_avec_commentaires_et_mains/ex-ft_itoa2.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: casomarr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/21 14:21:25 by casomarr          #+#    #+#             */
+/*   Updated: 2022/11/21 17:58:53 by casomarr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char reverse(char *string)
+{
+	int i;
+	int len;
+	char temp;
+
+	i = 0;
+	len = ft_strlen(string) - 1;
+	while (string[i])
+	{
+		temp = string[i];
+		string[i] = string[len];
+		string[len] = temp;
+		i++;
+		len--;
+	}
+	return(*string);
+}
+
+char *ft_itoa(int n)
+{
+	char *string;
+	int len; // longueur de l'int (nb de chiffres)
+	int i;
+	unsigned int nb;
+
+	nb = n;
+	len = 1; // car d�s que le nb est pile >=10 il faut rajouter 1
+	while (nb >= 10)
+	{
+		nb = nb / 10;
+		len++;
+	}
+	string = malloc(sizeof(char) * (len + 1));
+	if (string == NULL)
+		return (NULL);
+	i = 0;
+	if (n < 0)
+	{
+		string[i] = '-';
+		i++;
+	}
+	nb = n;
+	while (nb >= 10)
+	{
+		string[i] = nb % 10 + '0';
+		nb = nb / 10;
+		i++;
+	}
+	if (nb < 10)
+		string[i] = nb + '0';
+	string[i + 1] = '\0';
+	return(reverse(string));
+}
+
+
+int main()
+{
+	int n = 45;
+	int		len;
+
+	len = 0;
+	char *res;
+	res = malloc(sizeof(char) * 3);
+	res = ft_itoa(n);
+    while (res[len])
+		len++;
+	write(1, res, len);
+    free(res);
+
+	return(0);
+}
+
---
+++ libft/libft_avec_commentaires_et_mains/ex-ft_strchr2.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: casomarr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/09 14:21:01 by casomarr          #+#    #+#             */
+/*   Updated: 2022/11/21 16:56:05 by casomarr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	int		i;
+	char	*temp;
+
+	if (c == '\0')
+	{
+		temp = &((char *)s)[ft_strlen(s) + 1];
+		return (temp);
+	}
+	i = 0;
+	while (s[i] != c && s[i])
+	{
+		i++;
+	}
+	if (s[i] != '\0')
+	{
+		temp = &((char *)s)[i];
+		return (temp);
+	}
+	/*else if (s[i] != c && s[i + 1] == '\0')
+	{
+		temp = &((char *)s)[i + 1];
+		return (temp);
+	}*/
+	else
+		return (NULL);
+}
+
+/*
+int main ()
+{
+    const char str[] = "Tengo hambre";
+	//const char str[] = "\0";
+    //char c = 'e'; // marche
+    //char c = 'f'; // marche
+    char c = '\0'; //(doit retourner pointeur sur '\0' et non NULL). Marche.
+
+    // cas ou il y a la lettre dans str
+    //printf("%s\n", ft_strchr(str, c));
+    //printf("%s\n", strchr(str, c));
+
+    // cas ou on cherche un caractere pas la : renvoie NIL ( = pointeur sur NULL)
+    printf("%s\n", (char *)ft_strchr(str, c));
+    printf("%s\n", (char *)strchr(str, c));
+
+    // cas ou on cherche un caractere pas la : doit renvoyer l'adresse de \0. Ca marche :)
+    //printf("%p\n", ft_strchr(str, c));
+    //printf("%p\n", strchr(str, c));
+
+    return (0);
+}
+*/
